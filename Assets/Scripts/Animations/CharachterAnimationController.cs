ffd185223de8948b88367b474a0d9ffa38b46074
Adam <N00211418@iadt.ie>
2023-11-30 15:03:12 +0000

    fixed movement animations

--- Assets/Scripts/Animations/CharachterAnimationController.cs
+++ Assets/Scripts/Animations/CharachterAnimationController.cs
@@ -11,63 +11,116 @@ public class CharachterAnimationController : MonoBehaviour
     private Animator animator;
     public GameObject currentPlayer;
     private bool isGrounded;
+
+    private bool controllerJumpButton = false;
+    private bool keyboardJumpButton = true;
+
     private CharachterMovement charachterMovement;
-
-    private void setIsGrounded(bool grounded)
-    {
-        isGrounded = grounded;
-        Debug.Log( "grounded : " + grounded);
-    }
-
-    private void listenIsPlayerGrounded()
-    {
-        charachterMovement.OnGrounded += setIsGrounded;
-    }
+    private Rigidbody rigidBody;
 
 
-
-    // Start is called before the first frame update
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
         charachterMovement = currentPlayer.GetComponent<CharachterMovement>();
-        listenIsPlayerGrounded();
+        rigidBody = currentPlayer.GetComponent<Rigidbody>();
+    }
+
+    private void cancleJump()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
+    // KEYBOARD ANIMATIONS
+    // --------------------------------------------------------------
+    private void handleKeyboardMovement()
+    {
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.dKey.isPressed)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+    private void handleKeyboardJump(bool grounded)
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            if (rigidBody.velocity.y > -0.1f && grounded)
+            {
+                animator.SetBool("isJumping", true);
+                animator.SetBool("isGrounded", false);
+
+                Invoke("cancleJump", 1.0f);
+                keyboardJumpButton = true;
+            }
+        }
+
+        else if (grounded && rigidBody.velocity.y < -0.5f && keyboardJumpButton)
+        {
+            animator.SetBool("isGrounded", true);
+            animator.SetBool("isJumping", false);
+            keyboardJumpButton = false;
+        }
+    }
+
+    // CONTROLLER ANIMATIONS
+    // -------------------------------------------------------------
+    private void handleControllerMovement(bool grounded)
+    {
+        Vector2 leftStickInput = Gamepad.current.leftStick.ReadValue();
+
+        if (leftStickInput.x < -0.1f || leftStickInput.x > 0.1f)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else if (leftStickInput.x == 0 && grounded)
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+    private void handleControllerJump(bool grounded)
+    {
+        if (Gamepad.current.buttonSouth.wasPressedThisFrame)
+        {
+            if (rigidBody.velocity.y > -0.1f && grounded)
+            {
+                animator.SetBool("isJumping", true);
+                animator.SetBool("isGrounded", false);
+
+                Invoke("cancleJump", 1.0f);
+                controllerJumpButton = true;
+            }
+        }
+
+        else if (grounded && rigidBody.velocity.y < -0.5f && controllerJumpButton)
+        {
+            animator.SetBool("isGrounded", true);
+            animator.SetBool("isJumping", false);
+            controllerJumpButton = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current != null)
+
+        isGrounded = charachterMovement.IsGrounded();
+
+        if (Keyboard.current != null && currentPlayer.name == "Player1")
         {
-
-            if(Keyboard.current.aKey.isPressed || Keyboard.current.dKey.isPressed)
-            {
-                animator.SetBool("isRunning", true);
-            }
-
-            // if(Keyboard.current.aKey.wasReleasedThisFrame || Keyboard.current.dKey.wasReleasedThisFrame)
-            // {
-            //     animator.SetBool("isRunning", false);
-            // }
-
-            // if(Keyboard.current.spaceKey.wasPressedThisFrame)
-            // {
-            //     animator.SetBool("isJumping", true);
-            // }
-
-
-            //     animator.SetBool("isFalling", true);
-            //     animator.SetBool("isJumping", false);
-
-            //     if(isGrounded){
-            //         animator.SetBool("isGrounded", true);
-            //     }
-
+            handleKeyboardMovement();
+            handleKeyboardJump(isGrounded);
         }
 
-        if (Gamepad.current != null)
+        if (Gamepad.current != null && currentPlayer.name == "Player2")
         {
-           
+            handleControllerMovement(isGrounded);
+            handleControllerJump(isGrounded);
         }
     }
 }
